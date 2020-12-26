ea18e0f3a0ad91ed4e011e17ca6e580e2dbe2c95
vamsikrishna2k6 <vamsi.krishna2k6>
2020-12-25 16:45:41 -0800

    Variable Print

---
+++ Input.py
@@ -0,0 +1,5 @@
+input('please enter your name: ')
+
+
+name = input('Please Enter your Name:' )
+print('WelCome Ra:' + name)
--- Variables.py
+++ Variables.py
@@ -1,4 +1,6 @@
-age = 20
-first_name = "Josh"
-sex = "F"
-print(age,first_name,sex)
+
+
+name = "John Smith"
+age1 = 20
+new_patient = "yes"
+print("name:" + name, "age1:" + age1,new_patient:" + new_patient)
