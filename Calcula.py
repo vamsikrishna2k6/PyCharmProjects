6aa7a8946ec4c4c4bb90eb230d403e4f7bd5014d
vamsikrishna2k6 <vamsi.krishna2k6>
2020-12-25 17:23:14 -0800

    Cal

---
+++ Calcula.py
@@ -0,0 +1,14 @@
+first = input("Enter first number: " )
+second = input("Enter second number: ")
+add = float(first) + int(second)
+print("adding: " + str(add))
+
+first = input("Enter first number: " )
+second = input("Enter second number: ")
+add = float(first) + float(second)
+print("adding: " + str(add))
+
+first = float(input("Enter first number: "))
+second = float(input("Enter second number: "))
+add = first + second
+print("adding: " + str(add))
--- Converting.py
+++ Converting.py
@@ -1,3 +1,3 @@
 birth_year = input('Enter your birthyear: ')
 age = 2020 - int(birth_year)
-print(age)
+print("Your age is :" + str(age))
